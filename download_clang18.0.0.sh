ab2abcff8efb005f0199709482a73ee4d235b067
Akari <akariondev@gmail.com>
2023-10-23 21:24:08 -0300

    script: added script for downloading clang 18.0.0 by https://github.com/ZyCromerZ
    Signed-off-by: Akari <akariondev@gmail.com>

---
+++ download_clang18.0.0.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# -----------------------------
+#   By Akari Nyan - © 2023
+# ----------------------------- 
+
+url="https://github.com/ZyCromerZ/Clang/releases/download/18.0.0-20231023-release/Clang-18.0.0-20231023.tar.gz"
+
+output_file="Clang-18.0.0-20231023.tar.gz"
+destination_dir="$HOME/tc/clang-18.0.0"
+
+if ! command -v curl &>/dev/null; then
+  echo "The command 'curl' not installed. Please, install for continue."
+  sudo apt update -y 
+  sudo apt install curl -y
+  exit 1
+fi
+
+curl -LO "$url"
+
+if [ $? -ne 0 ]; then
+  echo "The download of the archive has failed."
+  exit 1
+fi
+
+mkdir -p "$destination_dir"
+tar -xzf "$output_file" -C "$destination_dir"
+
+if [ $? -eq 0 ]; then
+  echo "Archive extracted with success in '$destination_dir'."
+else
+  echo "The extraction of the zip was failed!"
+fi
+
