f634a0053d672e5c50d4ca29b572a711bf7fa216
明 <akariondev@gmail.com>
2024-01-08 17:18:55 -0300

    download_clang18.0.0.sh: update for new version.

--- download_clang18.0.0.sh
+++ download_clang18.0.0.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # -----------------------------
-#   By Akari Nyan - © 2023
+# script created by
+#     Akari Azusagawa © 2023
 # ----------------------------- 
 
-url="https://github.com/ZyCromerZ/Clang/releases/download/18.0.0-20231128-release/Clang-18.0.0-20231128.tar.gz"
+date_="20240106" # updated clang version.
+version="18.0.0git-$date_-release"
+clang_gz="Clang-18.0.0git-$date_.tar.gz"
+url="https://github.com/ZyCromerZ/Clang/releases/download/$version/$clang_gz"
 
-output_file="Clang-18.0.0-20231128.tar.gz"
+output_file="$clang_gz"
 destination_dir="$HOME/tc/clang-18.0.0"
 
 if ! command -v curl &>/dev/null; then
